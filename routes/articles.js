7d589644eb945643807139366b032b35ffeaf2b1
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2022-07-29 09:50:09 +0200

    Summery06
    creating Article schema and Database connection.

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
   res.render('articles/new.ejs')
 })
 
-router.post('/', (req, res) => {
-  
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', async(req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown
+  })
+  try { 
+    await article.save()
+    res.redirect(`/articles/${article.id}`)
+  } catch (e) {
+    res.render('articles/new', { article: article})
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://localhost/blog')
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
     const articles = [{
@@ -18,5 +18,8 @@ app.get('/', (req, res) => {
     }]
     res.render('articles/index', { articles: articles})
 })
+
+
+app.use('/articles', articleRouter) 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${3000}`))
